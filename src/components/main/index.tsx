f82e70b2484cf96e71a4e7bde8c0d052d3952953
Caio Dias <caiocozendey@hotmail.com>
2023-07-11 18:51:17 -0300

    key id

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -2,10 +2,7 @@ import Tech from "./tech";
 import Projetos from "./projetos";
 import Sobre from "./sobre";
 
-
-
-
-export default function () {
+function Main() {
     return (
         <div className="mx-auto max-w-6xl px-2 sm:px-2 lg:px-8">
             <Sobre />
@@ -14,3 +11,5 @@ export default function () {
         </div>
     )
 }
+
+export default Main;
--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -90,7 +90,7 @@ export default function Navbar() {
                                     href={item.href}
                                     as="a"
                                     className={classNames(
-                                        item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:text-[#34d399] duration-150',
+                                        item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:text-[#34d399]',
                                         'block rounded-md px-3 py-2 text-base font-medium'
                                     )}
                                     aria-current={item.current ? 'page' : false}
