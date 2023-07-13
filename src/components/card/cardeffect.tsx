b3587519c77c137f87579c3ebf8c6cb4e3b53fe5
Caio Dias <caiocozendey@hotmail.com>
2023-07-13 15:16:17 -0300

    Bug imagem projetos

--- src/components/card/cardeffect.tsx
+++ src/components/card/cardeffect.tsx
@@ -1,19 +1,26 @@
 
-interface Card{
+interface Card {
     text: string,
     name: string,
     image: string,
 }
 
 export default function Card(props: Card) {
+
+    let imageurl: any = props.image;
+
     return (
-        <div className="static rounded-lg duration-300 cursor-pointer w-48 h-72 bg-cover bg-no-repeat" style={{backgroundImage: `url(${props.image})` }}>
+        <div className="static rounded-lg duration-300 cursor-pointer w-48 h-72 bg-cover bg-no-repeat"
+            style={{
+                backgroundImage: `url(${imageurl})`,
+            }}
+        >
             <div className="w-full h-72 hover:bg-white hover:bg-opacity-5 rounded-lg hover:backdrop-blur-lg duration-300">
                 <div className="w-48 h-64 pt-2 text-transparent hover:text-white hover:blur-none duration-300 px-4">
                     <p>{props.text}</p>
                 </div>
                 <h1 className="pl-4 text-white text-xl text-bold hover:pl-24 duration-300">{props.name}</h1>
-            </div>            
+            </div>
         </div>
     )
 }
