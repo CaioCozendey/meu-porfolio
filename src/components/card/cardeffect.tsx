a1df25ae733927e96489e183857e8ee8bdabe745
Caio Dias <caiocozendey@hotmail.com>
2023-07-13 19:43:38 -0300

    font size nome projeto

--- src/components/card/cardeffect.tsx
+++ src/components/card/cardeffect.tsx
@@ -9,14 +9,14 @@ interface Card {
 export default function Card(props: Card) {
 
     return (
-        <div className="static rounded-lg duration-300 cursor-pointer w-48 h-72 bg-cover bg-no-repeat"
+        <div className="static rounded-lg duration-300 cursor-pointer w-48 h-72 bg-cover bg-no-repeat shadow-xl shadow-black"
             style={{ backgroundImage: `url(${props.image})`}}
         >
             <div className="w-full h-72 hover:bg-white hover:bg-opacity-5 rounded-lg hover:backdrop-blur-lg duration-300">
-                <div className="w-48 h-64 pt-2 text-transparent hover:text-white hover:blur-none duration-300 px-4">
+                <div className="w-48 h-60 pt-2 text-transparent hover:text-white hover:blur-none duration-300 px-4">
                     <p>{props.text}</p>
                 </div>
-                <h1 className="pl-4 text-white text-xl text-bold hover:pl-24 duration-300">{props.name}</h1>
+                <h1 className="pl-4 text-white text-2xl text-bold hover:pl-20 duration-300 align-bottom">{props.name}</h1>
             </div>
         </div>
     )
