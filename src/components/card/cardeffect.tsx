cf32603bc8f5e0ad921cb0b650b6de7baa0d5a75
Caio Dias Cozendey <41480153+CaioCozendey@users.noreply.github.com>
2023-09-05 10:33:52 -0300

    feat: add drop-shadow-md in class name <p> {props.text}

--- src/components/card/cardeffect.tsx
+++ src/components/card/cardeffect.tsx
@@ -14,7 +14,7 @@ export default function Card(props: Card) {
         >
             <div className="w-full h-72 hover:bg-white hover:bg-opacity-5 rounded-lg hover:backdrop-blur-lg duration-300">
                 <div className="w-48 h-60 pt-2 text-transparent hover:text-white hover:blur-none duration-300 px-4">
-                    <p>{props.text}</p>
+                    <p className="drop-shadow-md">{props.text}</p>
                 </div>
                 <h1 className="pl-4 text-white text-2xl text-bold hover:pl-20 duration-300 align-bottom">{props.name}</h1>
             </div>
